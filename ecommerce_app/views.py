48a0db9c18f36e669d54a15d8ad91c209b99ed64
Volga-Mohan <volgamohanan@gmail.com>
2021-02-24 21:09:07 +0530

    user-signin-signup

---
+++ ecommerce_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse
+
+
+def register_request(request):
+    form=NewUserForm()
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('register'))
+    return render (request=request, template_name="accounts/register.html",context={"register_form":form})
